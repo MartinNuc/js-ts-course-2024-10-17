019b541890058e4ce720bc90498c814dbb98230d
MartinNuc <martin.nuc@productboard.com>
2024-10-18 14:22:26 +0200

    calculator interface

--- typescript/calculator.ts
+++ typescript/calculator.ts
@@ -1,3 +1,15 @@
+interface Calculator {
+  add(a: number, b: number): number;
+  subtract(a: number, b: number): number;
+  PI: 3.14;
+}
+
+let calculator: Calculator = {
+  add,
+  subtract,
+  PI: 3.14,
+}
+
 export function add(a: number, b: number): number {
   return a + b;
 }
