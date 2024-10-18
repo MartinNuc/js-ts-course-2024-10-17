56ccc2eee58a8e7fee6b498909bd84b0dab01089
MartinNuc <martin.nuc@productboard.com>
2024-10-18 15:43:31 +0200

    remove key

--- typescript/remove-age.ts
+++ typescript/remove-age.ts
@@ -1,3 +1,21 @@
+function removeKey<T extends Record<string, unknown>, K extends keyof T>(
+  obj: T,
+  key: K
+): Omit<T, K> {
+  const duplicate = { ...obj };
+  delete duplicate[key];
+  return duplicate;
+}
+
+const result = removeKey(
+  {
+    id: 1,
+    name: "Martin",
+    height: 170,
+  },
+  "height"
+);
+
 type ObjectWithAge = {
   age: unknown;
   [key: string]: unknown;
